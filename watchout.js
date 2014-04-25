1e529ef38bb39a9496bd4f84324f952400079ed3
Hack Reactor Students <accounts@hackreactor.com>
2014-04-25 15:24:35 -0700

    Draw enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,25 @@
 // start slingin' some d3 here.
 
+// set width and height
+var width = 960;
+var height = 500;
+
+// append an svg to the body
+var svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+  .append('g');
+
+// add a certain number of enemies (circles)
+var enemies = svg.selectAll('.enemy')
+  .data([1, 2, 3, 4, 5]);
+
+// use enter().append() to add these enemies to the svg
+enemies.enter().append('circle')
+    .attr('class', 'enemy')
+    .attr('r', 10)
+    .attr('cx', function() {return Math.random()*(width-20)+10;})
+    .attr('cy', function() {return Math.random()*(height-20)+10;})
+
+// set attr x to Math.random()*width of svg element
+// set attr y to ""            height  ""
