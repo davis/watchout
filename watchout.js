5232c41be145500af73d3fe3f787fb3abce5e952
Hack Reactor Students <accounts@hackreactor.com>
2014-04-25 16:05:17 -0700

    Make a differently-colored dot to represent the player. Make it draggable

--- watchout.js
+++ watchout.js
@@ -11,6 +11,14 @@ var svg = d3.select('body').append('svg')
     .attr('height', height)
   .append('g');
 
+// good guy/hero
+var hero = svg.append('circle')
+    .attr('class', 'hero')
+    .attr('draggable', true)
+    .attr('r', 10)
+    .attr('cx', width/2)
+    .attr('cy', height/2);
+
 // add a certain number of enemies (circles)
 var enemies = svg.selectAll('.enemy')
   .data(new Array(numberOfEnemies));
@@ -21,8 +29,36 @@ enemies.enter().append('circle')
     .attr('r', 10)
     // set attr x to Math.random()*width of svg element
     .attr('cx', function() {return Math.random()*(width-20)+10;})
-    // set attr y to ""            height  ""
+    // set attr y to '            height  '
     .attr('cy', function() {return Math.random()*(height-20)+10;});
+
+var drag = d3.behavior.drag()
+  .on('dragstart', dragstart)
+  .on('drag', dragged)
+  .on('dragend', dragend);
+d3.selectAll('.hero').call(drag);
+
+function dragstart() {
+  d3.event.sourceEvent.stopPropagation();
+  d3.select(this).classed('dragging', true);
+}
+
+function dragged() {
+  d3.select(this)
+    .attr('cx', d3.event.x)
+    .attr('cy', d3.event.y)
+}
+
+function dragend() {
+  d3.select(this).classed('dragging', false);
+}
+
+
+
+
+
+
+
 
 function update(data) {
   enemies
